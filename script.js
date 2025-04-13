ae0e9af5bddab2dec10fcb6811f7baca3960e6d7
fideprotaofc <lindeberguidasilvadelimalindeb@gmail.com>
2025-04-12 22:30:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const startDate = new Date("2024-10-26");
-const today = new Date();
-const rose = document.getElementById("rose");
-
-const days = Math.floor((today - startDate) / (1000 * 60 * 60 * 24));
-const size = 50 + days * 2;
-
-rose.style.width = `${size}px`;
-rose.style.height = `${size}px`;
